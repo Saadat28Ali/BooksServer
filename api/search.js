30c339aecf3b37b45954b35cf29921ea2f7d43d2
unknown <ali.saadat.1003@gmail.com>
2024-10-30 21:04:50 +0530

    integrated serverFetch script to the main api script

--- api/search.js
+++ api/search.js
@@ -2,7 +2,19 @@
 import { searchBooksBy } from "../public/scripts/serverFetch.js";
 
 function GET(request, response) {
-    response.status(200).send("Hello");
+    
+    const searchBy = request.query.searchby;
+    const searchValue = request.query.value;
+
+    if (["title", "author", "category"].includes(searchBy)) {
+        searchBooksBy(searchBy, RegExp("^"+searchValue, "i")).then(
+            (result) => {
+                response.status(200).send(result);
+            }
+        )
+    }
+    
+    response.status(400).send("Error occurred");
 }
 
 export default GET;

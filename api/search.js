f9f154b33e1a5bc8daaf2f25e48e0860d9e46f12
unknown <ali.saadat.1003@gmail.com>
2024-10-30 21:43:54 +0530

    .

--- api/search.js
+++ api/search.js
@@ -1,19 +1,26 @@
 // script imports
 import { searchBooksBy } from "../public/scripts/serverFetch.js";
 
-function GET(request, response) {
+async function GET(request, response) {
     
     const searchBy = request.query.searchby;
     const searchValue = request.query.value;
 
+
     if (["title", "author", "category"].includes(searchBy)) {
-        searchBooksBy(searchBy, RegExp("^"+searchValue, "i")).then(
-            (result) => {
-                return response.status(200).send(result);
-            }
-        )
+
+        const result = await searchBooksBy(searchBy, RegExp("^"+searchValue, "i"));
+        response.status(200).send(result);
+
+        // searchBooksBy(searchBy, RegExp("^"+searchValue, "i")).then(
+        //     (result) => {
+        //         return response.status(200).send(result);
+        //     }
+        // )
+
+
     }
-    return response.status(200).send("HERE");
+    // return response.status(200).send("HERE");
 }
 
 export default GET;

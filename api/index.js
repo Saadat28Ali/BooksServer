0dc8d777a7acef72a472c2871bdf10b45402cd2d
unknown <ali.saadat.1003@gmail.com>
2024-10-30 09:38:41 +0530

    made it so that the result of the db query is returned as json

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@
 import express from "express";
 
 // script imports
-import {searchBooksBy} from "../public/scripts/serverFetch.js";
+import { searchBooksBy } from "../public/scripts/serverFetch.js";
 
 const port = 5000;
 const app = express();
@@ -12,17 +12,14 @@ app.get("/api/", (request, response) => {
     const searchBy = request.query.searchby;
     const searchValue = request.query.value;
 
-    console.log(searchBy, searchValue);
-
     if (["title", "author", "category"].includes(searchBy)) {
         searchBooksBy(searchBy, RegExp("^"+searchValue, "i")).then(
             (result) => {
                 console.log(result);
+                response.json(result);
             }
         )
     }
-
-    response.send("This is the home");
 })
 
 app.listen(port, () => {console.log(`Server listening on port: ${port}`)});

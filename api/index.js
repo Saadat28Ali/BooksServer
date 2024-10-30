2c01ec93dce3921584a8fd27eea20af147b2aec7
unknown <ali.saadat.1003@gmail.com>
2024-10-30 12:12:03 +0530

    added status code to express app

--- api/index.js
+++ api/index.js
@@ -25,7 +25,7 @@ app.get("/api/", (request, response) => {
         searchBooksBy(searchBy, RegExp("^"+searchValue, "i")).then(
             (result) => {
                 console.log(result);
-                response.json(result);
+                response.status(200).json(result);
             }
         )
     }

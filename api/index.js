0c75c2117ebfbad91ac02afcbea11ac53f4056ce
unknown <ali.saadat.1003@gmail.com>
2024-10-30 10:54:11 +0530

    allowed CORS

--- api/index.js
+++ api/index.js
@@ -7,10 +7,19 @@ import { searchBooksBy } from "../public/scripts/serverFetch.js";
 const port = 5000;
 const app = express();
 
+app.use((req, res, next) => {
+    res.append('Access-Control-Allow-Origin', ['*']);
+    res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.append('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
+
 app.get("/api/", (request, response) => {
 
     const searchBy = request.query.searchby;
     const searchValue = request.query.value;
+
+    console.log(searchBy, searchValue);
 
     if (["title", "author", "category"].includes(searchBy)) {
         searchBooksBy(searchBy, RegExp("^"+searchValue, "i")).then(
